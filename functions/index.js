88a80a4163740ad4d8219970fb02bdbcf9cd2423
Joshua Colley <>
2020-02-11 21:49:43 +0000

    update index.js file

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,11 @@
 const functions = require('firebase-functions');
+const app = require('express')()
+const cors = require('cors')()
+const bodyParser = require('body-parser')
+const router = require('./api/routers/router')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.use(cors)
+app.use(bodyParser)
+router(app)
+
+exports.api = functions.https.onRequest(app)
